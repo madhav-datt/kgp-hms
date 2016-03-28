e895eb5b627606021bdd8e9c7e1acbe92cbd3588
madhav-datt <madhav.datt@hotmail.com>
2016-03-29 00:57:19 +0530

    add getter and setter functions

--- src/actors/student.py
+++ src/actors/student.py
@@ -5,7 +5,6 @@ class Student(object):
         student_ID: An integer to uniquely identify student
         password: A string hashed after adding salt
         name: string
-        gender: string
         address: string
         contact_number: string
         photograph: image
@@ -24,7 +23,6 @@ class Student(object):
         self.student_ID = student_ID
         self.password = password
         self.name = name
-        self.gender = gender
         self.address = address
         self.contact_number = contact_number
         self.photograph = photograph
@@ -35,3 +33,84 @@ class Student(object):
         # Total dues payable by student
         # total_dues = room_rent + mess_charges + amenities_charges
         self.total_dues = 0.
+
+    # student_ID getter and setter functions
+    @property
+    def student_ID(self):
+        return self._student_ID
+
+    @student_ID.setter
+    def student_ID(self, student_ID):
+        self._student_ID = student_ID
+
+    # password getter and setter functions
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    # name getter and setter functions
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    # address getter and setter functions
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, address):
+        self._address = address
+
+    # contact_number getter and setter functions
+    @property
+    def contact_number(self):
+        return self._contact_number
+
+    @contact_number.setter
+    def contact_number(self, contact_number):
+        self._contact_number = contact_number
+
+    # photograph getter and setter functions
+    @property
+    def photograph(self):
+        return self._photograph
+
+    @photograph.setter
+    def photograph(self, photograph):
+        self._photograph = photograph
+
+    # hall_ID getter and setter functions
+    @property
+    def hall_ID(self):
+        return self._hall_ID
+
+    @hall_ID.setter
+    def hall_ID(self, hall_ID):
+        self._hall_ID = hall_ID
+
+    # room_no getter and setter functions
+    @property
+    def room_no(self):
+        return self._room_no
+
+    @room_no.setter
+    def room_no(self, room_no):
+        self._room_no = room_no
+
+    # room_type getter and setter functions
+    @property
+    def room_type(self):
+        return self._room_type
+
+    @room_type.setter
+    def room_type(self, room_type):
+        self._room_type = room_type
