9cf899eb2b5c4e919cbbefaa0deddf8b93b569d3
madhav-datt <madhav.datt@hotmail.com>
2016-03-29 10:29:52 +0530

    add dependancy installations

---
+++ src/install_dep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Script to download and install software and database required for HMS
+# Software Engineering Lab - Assignment 5
+# IIT Kharagpur - Hall Management System
+#
+# authors: Madhav Datt (14CS30015), Avikalp Srivastava (14CS10008)
+#
+
+# Install MySQL Server
+apt-get install mysql-server
