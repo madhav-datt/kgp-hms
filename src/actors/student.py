e491cf1620ec9c34c24a18df5a34d5e1d8ef1dc5
madhav-datt <madhav.datt@hotmail.com>
2016-03-29 09:13:28 +0530

    add db connection

--- src/actors/student.py
+++ src/actors/student.py
@@ -1,3 +1,15 @@
+#
+# Software Engineering Lab - Assignment 5
+# IIT Kharagpur - Hall Management System
+#
+
+"""
+@ authors: Madhav Datt (14CS30015), Avikalp Srivastava (14CS10008)
+"""
+
+from __future__ import division
+import warnings
+
 class Student(object):
     """Contains details of Student
 
