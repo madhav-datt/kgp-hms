6c7c6af4cf9d3db31e0cdbf61b660ae87a8ffe5e
madhav-datt <madhav.datt@hotmail.com>
2016-03-29 11:02:30 +0530

    add dependancy installations

--- src/install_dep.sh
+++ src/install_dep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Script to download and install software and database required for HMS
+# Must be run as superuser with sudo
+#
 # Software Engineering Lab - Assignment 5
 # IIT Kharagpur - Hall Management System
 #
@@ -8,4 +10,18 @@
 #
 
 # Install MySQL Server
+# Will be prompted to enter and confirm root password
+echo "Installing MySQL Server"
+echo ""
 apt-get install mysql-server
+
+# Start MySQl service/Ensure service is running
+service mysql start
+/usr/sbin/update-rc.d mysql defaults
+
+# Address security concerns in default MySQL installation
+# For use in production environment: Recommended answers N Y Y Y Y to run all
+# required parts of the script
+mysql_secure_installation
+
+echo ""
