bb5ba44a254fc6484b6a42e554be6d12fd741f9f
madhav-datt <madhav.datt@hotmail.com>
2016-03-31 21:53:00 +0530

    add warden class

---
+++ src/actors/warden.py
@@ -0,0 +1,78 @@
+#
+# Software Engineering Lab - Assignment 5
+# IIT Kharagpur - Hall Management System
+#
+
+"""
+@ authors: Madhav Datt (14CS30015), Avikalp Srivastava (14CS10008)
+"""
+
+from __future__ import division
+import warnings
+
+class Warden(object):
+    """Contains details of Warden
+
+    Attributes:
+        warden_ID: Integer to uniquely identify warden
+        password: Hashed string after adding salt
+        name: String
+        email: String
+        hall_ID: Integer to identify hall of residence
+    """
+
+    def __init__(self, warden_ID, password, name, email, hall_ID):
+        """
+        Init Warden with details for object creation
+        """
+
+        self.warden_ID = warden_ID
+        self.password = password
+        self.name = name
+        self.email = email
+        self.hall_ID = hall_ID
+
+    # warden_ID getter and setter functions
+    @property
+    def warden_ID(self):
+        return self._warden_ID
+
+    @warden_ID.setter
+    def warden_ID(self, warden_ID):
+        self._warden_ID = warden_ID
+
+    # password getter and setter functions
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    # name getter and setter functions
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    # email getter and setter functions
+    @property
+    def address(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    # hall_ID getter and setter functions
+    @property
+    def hall_ID(self):
+        return self._hall_ID
+
+    @hall_ID.setter
+    def hall_ID(self, hall_ID):
+        self._hall_ID = hall_ID
