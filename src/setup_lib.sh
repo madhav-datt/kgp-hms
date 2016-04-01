8517f520e8d8beba20da2788fcb1de40f710795c
madhav-datt <madhav.datt@hotmail.com>
2016-04-01 07:36:12 +0530

    add library setup script

---
+++ src/setup_lib.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Script to download and install miscellaneous libraries required for HMS
+# Must be run as superuser with sudo
+#
+# Software Engineering Lab - Assignment 5
+# IIT Kharagpur - Hall Management System
+#
+# authors: Madhav Datt (14CS30015), Avikalp Srivastava (14CS10008)
+#
+
+# Install bcrypt for password hashing and authentication
+apt-get install python.bcrpyt
+
+# Install PyPDF2 for handling PDF printing
+apt-get install pyhton.pypdf2
+
+# Install MySQL Python Connector API
+wget -P ~/Downloads http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python_2.1.3-1ubuntu15.04_all.deb
+cd Downloads
+dpkg -i mysql-connector-python_2.1.3-1ubuntu15.04_all.deb
+rm mysql-connector-python_2.1.3-1ubuntu15.04_all.deb
