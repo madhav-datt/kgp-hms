25f32b26cfa14b4f4b63189effe569e5cba8ec2f
madhav-datt <madhav.datt@hotmail.com>
2016-04-03 14:02:37 +0530

    add worker class files

---
+++ src/workers/worker.py
@@ -0,0 +1,11 @@
+#
+# Software Engineering Lab - Assignment 5
+# IIT Kharagpur - Hall Management System
+#
+
+"""
+@ authors: Madhav Datt (14CS30015), Avikalp Srivastava (14CS10008)
+"""
+
+from __future__ import division
+from database import db_func as db
