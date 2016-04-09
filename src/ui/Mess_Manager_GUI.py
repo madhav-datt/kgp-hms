c02d7d6d6a5f44748324916cceaeeb9b7a7f11e6
Avikalp7 <avikalp22@gmail.com>
2016-04-09 23:24:44 +0530

    vinso

--- src/ui/Mess_Manager_GUI.py
+++ src/ui/Mess_Manager_GUI.py
@@ -14,10 +14,10 @@ class MessManagerWindowClass(QtGui.QWidget, Mess_Manager_Window.Ui_Form):
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.setupUi(self)
-        self.label_7.setPixmap(QtGui.QPixmap(_fromUtf8('bkd1edit2.jpg')))
+        self.label_7.setPixmap(QtGui.QPixmap(_fromUtf8('src/ui/bkd1edit2.jpg')))
         self.label_7.setScaledContents(True)
 
-        self.label_12.setPixmap(QtGui.QPixmap(_fromUtf8('bkd1edit2.jpg')))
+        self.label_12.setPixmap(QtGui.QPixmap(_fromUtf8('src/ui/bkd1edit2.jpg')))
         self.label_12.setScaledContents(True)
 
 app = QApplication(sys.argv)
