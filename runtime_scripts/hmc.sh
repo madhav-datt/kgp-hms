42d749cd645452af2e52821d5ea4db813b7c16a7
madhav-datt <madhav.datt@hotmail.com>
2016-04-24 18:51:57 +0530

    add program execute scripts

---
+++ runtime_scripts/hmc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script to run HMC System
+# Must be run as superuser with sudo
+#
+# IIT Kharagpur - Hall Management System
+# System to manage Halls of residences, Warden grant requests, student complaints
+# hall worker attendances and salary payments
+#
+# authors: Madhav Datt, Avikalp Srivastava
+# MIT License
+#
+
+cd /etc/kgp-hms
+python -m src.ui.HMC_GUI
+
